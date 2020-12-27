f9bd200b5f140e21a44796a10ccf8a906276223e
松野浩士 <amanojac52@gmail.com>
2020-12-27 19:48:13 +0900

    react-router設定、履歴へのリンク

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,34 @@
-import * as React from "react";
-import { render } from "react-dom";
-import styled from "styled-components";
-import { createGlobalStyle } from "styled-components";
-import { Editor } from "./pages/editor";
+import * as React from "react"
+import { render } from "react-dom"
+import styled from "styled-components"
+import { createGlobalStyle } from "styled-components"
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import { Editor } from "./pages/editor"
+import { History } from './pages/history'
 
 const GlobalStyle = createGlobalStyle`
   body * {
     box-sizing: border-box;
   }
-`;
+`
 
 const Main = (
   <>
     <GlobalStyle />
-    <Editor />
+    <Router>
+      <Route exact path="/editor">
+        <Editor />
+      </Route>
+      <Route exact path="/history">
+        <History/>
+      </Route>
+      <Redirect to="/editor" path="*"/>
+    </Router>
   </>
 );
 render(Main, document.getElementById("app"));
