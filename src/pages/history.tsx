7ead2fae1f80d0a637cd10b29d2d3bcb2cbccccc
松野浩士 <amanojac52@gmail.com>
2020-12-27 21:33:23 +0900

    履歴の取得と表示

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
-import {
-  Link,
-  useHistory
-} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { Header } from '../components/header'
+import {
+  getMemos,
+  MemoRecord,
+} from '../indexeddb/memos'
+
+const { useState, useEffect } = React
 
 const HeaderArea = styled.div`
   position: fixed;
@@ -21,7 +24,36 @@ const Wrapper = styled.div`
   top: 3rem;
   padding: 0 1rem;
 `
+
+const Memo = styled.button`
+  display: block;
+  background-color: white;
+  border: 1px solid gray;
+  width: 100%;
+  padding: 1rem;
+  margin 1rem 0;
+  text-align: left;
+`
+
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
 export const History: React.FC = () => {
+  const [memos, setMemos] = useState<MemoRecord[]>([])
+
+  // 副作用フック(effect hook)　レンダリングの後に実行される
+  useEffect(() => {
+    getMemos().then(setMemos)
+  }, [])
   return (
     <>
       <HeaderArea>
@@ -32,7 +64,12 @@ export const History: React.FC = () => {
         </Header>
       </HeaderArea>
       <Wrapper>
-        TODO: 履歴表示
+        {memos.map(memo => (
+          <Memo key={memo.datetime}>
+            <MemoTitle>{memo.title}</MemoTitle>
+            <MemoText>{memo.text}</MemoText>
+          </Memo>
+        ))}
       </Wrapper>
     </>
   )
